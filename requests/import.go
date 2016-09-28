9dc0de0ddaecae53e0681f69d003cb1b8d68e66c
Stephan Williams <stephanwilliams.93@gmail.com>
2016-09-28 15:27:23 -0400

    Add more collection import options

--- requests/import.go
+++ requests/import.go
@@ -1,15 +1,50 @@
 package requests
 
-import "fmt"
-
 // ImportCollection imports data to a collection
 type ImportCollection struct {
 	CollectionName string
 	Data           []byte
+	FromPrefix     string
+	ToPrefix       string
+	Overwrite      bool
+	WaitForSync    bool
+	OnDuplicate    string
+	Complete       bool
+	Details        bool
 }
 
 func (c *ImportCollection) Path() string {
-	return fmt.Sprintf("/_api/import/?type=auto&collection=%s", c.CollectionName)
+	path := "/_api/import/?type=auto&collection=" + c.CollectionName
+
+	if c.FromPrefix != "" {
+		path += "&fromPrefix=" + c.FromPrefix
+	}
+
+	if c.ToPrefix != "" {
+		path += "&toPrefix=" + c.ToPrefix
+	}
+
+	if c.Overwrite {
+		path += "&overwrite=yes"
+	}
+
+	if c.WaitForSync {
+		path += "&waitForSync=yes"
+	}
+
+	if c.OnDuplicate != "" {
+		path += "&onDuplicate=" + c.OnDuplicate
+	}
+
+	if c.Complete {
+		path += "&complete=yes"
+	}
+
+	if c.Details {
+		path += "&details=yes"
+	}
+
+	return path
 }
 
 func (c *ImportCollection) Method() string {
